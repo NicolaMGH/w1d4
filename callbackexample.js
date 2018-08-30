eab26b694cda6a234ea7792ee4472c7c6e3c429c
Nicola <va3nky@hotmail.com>
2018-08-30 16:59:06 +0000

    implemented anon function

--- callbackexample.js
+++ callbackexample.js
@@ -9,4 +9,11 @@ function runner(f) {
   f();
 }
 
-runner(myFn);
+runner(myFn
+
+
+
+//another way to do the waldo callback function
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], function(result) {
+  console.log("The result is:", result);
+});
